e631f23ffd55d597010e89fd0cfab5c479d3d351
Andrey Borodin <flyer29@bk.ru>
2020-09-11 13:48:19 +0300

    удалил crash-test

--- app.js
+++ app.js
@@ -40,12 +40,6 @@ app.use(requestLogger);
 app.use('/cards', auth, cardsRouter);
 app.use('/users', auth, usersRouter);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
